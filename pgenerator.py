ca4e0abda6147a79e1feedff00dd935f2e8744aa
obulesu-oba <69957872+obulesu-oba@users.noreply.github.com>
2020-11-27 00:51:14 +0530

    Update pgenerator.py

--- pgenerator.py
+++ pgenerator.py
@@ -1,15 +1,5 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import random
-
-
-# In[3]:
-
-
 alpha = ["A","C","D","F","H"]
 number = [1,10,5,6,7,8]
 small = ["a","f","r","h"]
@@ -17,10 +7,3 @@ char = ["!","@","^","*"]
 ran1 = random.choice(alpha)+str(random.choice(number))+random.choice(small)+random.choice(char)
 ran2 = random.choice(alpha)+str(random.choice(number))+random.choice(small)+random.choice(char)
 print(ran1+ran2)
-
-
-# In[ ]:
-
-
-
-
